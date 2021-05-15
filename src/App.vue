60b1baf0bcdbd2665efb516870555d2031645cd4
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-05-15 15:47:04 +0100

    pre graphql integration

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <NavBar />
-  <router-view />
+  <router-view v-on:scroll="handleScroll" />
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
 export default {
   components: {
-    NavBar
+    NavBar,
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll(e) {
+      const nav = document.querySelector(".main-nav");
+      window.scrollY > 10
+        ? (nav.style.backgroundColor = "white")
+        : (nav.style.backgroundColor = "rgb(240,240,224)");
+    },
   },
 };
 </script>
-
